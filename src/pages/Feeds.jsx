a31a0459a878a61b09f4b1456ec169d0af296591
heatherd2025 <heatherdeliso@gmail.com>
2026-02-01 17:20:35 -0500

    search bar implementation

--- src/pages/Feeds.jsx
+++ src/pages/Feeds.jsx
@@ -1,29 +1,28 @@
-import NewsFeed from '../components/NewsFeed.jsx';
+import NewsFeed from "../components/NewsFeed/NewsFeed.jsx";
 // import APOD from '../components/APOD.jsx';
-import APODCarousel from "../components/APODCarousel";
-import '../styles/feeds.css';
+import APODCarousel from "../components/NewsFeed/APODCarousel.jsx";
+import "../styles/feeds.css";
 
 const Feeds = () => {
   return (
     <>
-    <div className="mainNewsImagesContainer">
+      <div className="mainNewsImagesContainer">
+        <div className="col-6 col-lg-8">
+          <APODCarousel />
+        </div>
 
-              <div className="col-6 col-lg-4">
-                {/* <div className="row">
-                  <p className="feedHeader">QwRky News</p>
-              </div> */}
-                <NewsFeed />
-              </div>
+        <div className="col-6 col-lg-4">
+          {/*  <div className="row">
+                <p className="feedHeader">QwRky News</p>
+             </div> */}
+          <NewsFeed />
+        </div>
+      </div>
 
-              <div className="col-6 col-lg-8 apodContainer">
-                <APODCarousel/>
-              </div>
-    </div>
-
-        <footer>
-          <p className="footer">© {new Date().getFullYear()} Heather DeLiso</p>
-        </footer>
-   </>
+      <footer>
+        <p className="footer">© {new Date().getFullYear()} Heather DeLiso</p>
+      </footer>
+    </>
   );
 };
 
